a9d1d75d458f6db75a511d871324119381b36f4f
Douglas-Silverman <53188336+Douglas-Silverman@users.noreply.github.com>
2020-06-27 01:25:49 -0400

    comments on graph.py

--- graph.py
+++ graph.py
@@ -2,7 +2,26 @@ import matplotlib.pyplot as plt
 from run_model import * 
 from game_reader import *
 
+
+###############################################################
+# func: plotSport
+#
+#   -this function runs the simulation and plots the results for a parameterized sport and model
+#
+######### Parameters:
+#
+# | Parameter | Data Type | Description                                          | Example                          |
+# | --------- | --------- | ---------------------------------------------------- | -------------------------------- |
+# | title     | string    | The title for the graph                              | title : "NFL Poisson Martingale" |
+# | sport     | string    | Which sport should be simulated                      | sport : "NFL"                    |
+# | function  | function  | The model used in the simulation (found in model.py) | function : poisson_martingale    |
+#
+######### Returns:
+#
+# nothing is returned
+
 def plotSport(title, sport, function) :
+    # load file names
     files = []
     if(sport == "NFL") :
         files.append("./nfl odds/nfl odds 2017-18.csv")
@@ -17,15 +36,15 @@ def plotSport(title, sport, function) :
         files.append("./mlb odds/mlb odds 2018.csv")
         files.append("./mlb odds/mlb odds 2019.csv")
     
-    count = 0
-    yearPlot = plt.figure(figsize= ((6.4, 4.8)))
-    for year in files :
-        result = function(year, True, 100)
+    count = 0 # keeps track of current season 
+    yearPlot = plt.figure(figsize= ((6.4, 4.8))) # set figure size
+    for year in files : 
+        result = function(year, True, 100) # run simulation
         xVars = []
         scores = []
-    
+        # add data points to plot
         for currentTuple in result :
-            xVars.append(currentTuple[0])
+            xVars.append(currentTuple[0]) 
             scores.append(currentTuple[1])
         mark = "r"
         label = "2017 season"
@@ -35,14 +54,9 @@ def plotSport(title, sport, function) :
         if(count == 2) :
             mark = "g"
             label = "2019 season"
-        yearPlot = plt.plot(xVars, scores, mark, label = label)
-        scoreShort = []
-        for score in scores :
-            scoreShort.append(round(score, 2))
-        # for z in zip(xVars, scoreShort) :
-        #     plt.annotate(("(%s, %s)"%z), xy = z, textcoords = "data") 
-        count = count + 1
-    plt.xlabel("games")
+        yearPlot = plt.plot(xVars, scores, mark, label = label) # plot the data points
+        count = count + 1 # update year
+    plt.xlabel("games") 
     plt.ylabel("profit")
     plt.title(title)
     plt.grid(True)
